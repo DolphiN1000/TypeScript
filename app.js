e68fd16680167f0a448a726ed3a005ccc001ba52
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2023-07-20 19:14:50 +0300

    123

--- app.js
+++ app.js
@@ -1,9 +1,91 @@
-var button = document.querySelector("button");
-var input1 = document.getElementById("num1");
-var input2 = document.getElementById("num2");
-function add(num1, num2) {
-    return num1 + num2;
-}
-button.addEventListener("click", function () {
-    console.log(add(+input1.value, +input2.value));
-});
+//-----Lesson 1-----
+// const button = document.querySelector("button");
+// const input1 = document.getElementById("num1")! as HTMLInputElement;
+// const input2 = document.getElementById("num2")! as HTMLInputElement;
+// function add(num1: number, num2: number) {
+//   return num1 + num2;
+// }
+// button.addEventListener("click", function () {
+//   console.log(add(+input1.value, +input2.value));
+// });
+// -----lesson 2 -----
+// let age: number;
+// let name1: string;
+// let toggle: boolean;
+// let empty: null;
+// let notInitialize: undefined;
+// let callback = (a: number) => number;
+// age = 50;
+// name1 = "Max";
+// toggle = true;
+// empty = null;
+// notInitialize = undefined;
+// callback = (a) => {
+//   return 100 + a;
+// };
+// let anything: any;
+// anything = -20;
+// anything = "Text";
+// anything = {};
+// let some: unknown;
+// some = "Text";
+// let str: string;
+// if (typeof some === "string") {
+//   str = some;
+// }
+// let person: [string, number];
+// person = ["Max", 21];
+// enum Load {
+//   LOADING,
+//   READY,
+// }
+// const page = {
+//   load: Load.READY,
+// };
+// if (page.load === Load.LOADING) {
+//   console.log("Page loadiung");
+// }
+// if (page.load === Load.READY) {
+//   console.log("Page loaded");
+// }
+// let union1: string | number;
+// union1 = "alex";
+// union1 = 21;
+// let status1: "enable" | "disable";
+// function showMessage(message: string):void {
+//   console.log(message);
+// }
+// function calc(num1: number, num2: number): number {
+//   return num1 + num2;
+// }
+// function customError(): never {
+//   throw new Error("Error");
+// }
+// type DataPage = {
+//   title: string;
+//   likes: number;
+//   accounts: string[];
+//   status: "open" | "close";
+//   details?: {
+//     createAt: Date;
+//     updateAt: Date;
+//   };
+// };
+// const page1: DataPage = {
+//   title: "The awesome page",
+//   likes: 100,
+//   accounts: ["Max", "Anton", "Nikita"],
+//   status: "open",
+//   details: {
+//     createAt: new Date("2021-01-01"),
+//     updateAt: new Date("2021-05-01"),
+//   },
+// };
+// const page2: DataPage = {
+//   title: "Python o Js",
+//   likes: 5,
+//   accounts: ["Alex"],
+//   status: "close",
+// };
+// -----Lesson 3------
+// -----test-----
